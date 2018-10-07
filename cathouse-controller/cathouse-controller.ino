a32c5c51cddbbb3ebe0a9ac7952785cedef5260c
Lorenzo Delana <oss.devel@searchathing.com>
2018-10-07 04:02:17 +0200

    vscode settings ( see also https://github.com/devel0/knowledge/blob/7adb588b7419af2d8cc202991c49e793ff51a962/arduino/vscode-arduino.md ) ; temperature sensor

--- cathouse-controller/cathouse-controller.ino
+++ cathouse-controller/cathouse-controller.ino
@@ -1,7 +1,11 @@
+#define ARDUINO 10807
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiServer.h>
 #include <ESP8266HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 //------------------------------------------
 //
@@ -11,15 +15,18 @@
 #include "/home/devel0/security/wifi.h"
 
 #define DEBUG // comment to disable serial output
+#define TEMPERATURE_INTERVAL_MS 5000
+#define TEMPERATURE_DEVICE_COUNT 2
+#define ONE_WIRE_BUS D3
 
 //
 //------------------------------------------
 
 #ifdef DEBUG
-#define DEBUG_PRINT(x)     Serial.print (x)
-#define DEBUG_PRINTHEX(x)     Serial.print (x,HEX)
-#define DEBUG_PRINTF(x, args...)     Serial.printf (x, args)
-#define DEBUG_PRINTLN(x)  Serial.println (x)
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTHEX(x) Serial.print(x, HEX)
+#define DEBUG_PRINTF(x, args...) Serial.printf(x, args)
+#define DEBUG_PRINTLN(x) Serial.println(x)
 #else
 #define DEBUG_PRINT(x)
 #define DEBUG_PRINTF(x, args...)
@@ -35,23 +42,32 @@ int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
 
-//int pins[] = { D0, D1, D2, D3, /*D4,*/ D5, D6 };
 #define RELAY_ON LOW
 #define RELAY_OFF HIGH
 
-void setup() {  
-  Serial.begin(115200);  
-    
-  if (WiFi.status() == WL_NO_SHIELD) {
-    DEBUG_PRINTLN("WiFi shield not present");    
-    while (true);
+OneWire tempOneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&tempOneWire);
+DeviceAddress tempDevAddress[TEMPERATURE_DEVICE_COUNT]; // DeviceAddress defined as uint8_t[8]
+int temperatureDeviceCount = 0;
+float *temperatures = NULL;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    DEBUG_PRINTLN("WiFi shield not present");
+    while (true)
+      ;
   }
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     DEBUG_PRINT(".");
   }
-    
+
   printCurrentNet();
   printWifiData();
 
@@ -59,25 +75,59 @@ void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, LOW);
 
+  SetupTemperatureDevices();
+
   server.begin();
+}
+
+void SetupTemperatureDevices()
+{
+  DS18B20.begin();
+  temperatureDeviceCount = DS18B20.getDeviceCount();
+  DEBUG_PRINTF("temperature device count = %d\n", temperatureDeviceCount);
+  if (temperatureDeviceCount > 0)
+    temperatures = new float[temperatureDeviceCount];
+  for (int i = 0; i < temperatureDeviceCount; ++i)
+  {
+    DS18B20.getAddress(tempDevAddress[i], i);
+    DS18B20.setResolution(12);
+  }
+  ReadTemperatures();
+}
+
+long lastTemperatureRead;
+
+void ReadTemperatures()
+{
+  //DEBUG_PRINTLN("reading temperatures");
+  DS18B20.requestTemperatures();
+  for (int i = 0; i < temperatureDeviceCount; ++i)
+  {
+    auto temp = DS18B20.getTempC(tempDevAddress[i]);
+    DEBUG_PRINTF("temperature sensor %d = %f\n", i, temp);
+    temperatures[i] = temp;
+  }
+  lastTemperatureRead = millis();
 }
 
 String header;
 bool foundcmd;
 
-void loop() { 
+void loop()
+{
   auto client = server.available();
 
   if (client)
   {
     DEBUG_PRINTLN("new client");
-    String currentLine = "";    
+    String currentLine = "";
 
     while (client.connected())
     {
       if (client.available())
       {
         char c = client.read();
+        DEBUG_PRINTF("read char [%c]\n", c);
         header += c;
         if (c == '\n')
         {
@@ -89,83 +139,96 @@ void loop() {
             client.println();
 
             foundcmd = false;
-/*
-            for (int i = 0; i < 6; ++i)
+
+            String header_search;
+            header_search += "GET /";
+            if (temperatureDeviceCount > 0)
             {
-              String header_search;
-              pintmpstr[0] = '0' + i;
-              header_search += "GET /";
-              header_search += pintmpstr;
-
-              if (header.indexOf(header_search + "/on") >= 0)
+              for (int i = 0; i < temperatureDeviceCount; ++i)
               {
-                digitalWrite(pins[i], RELAY_ON); foundcmd = true;
-                DEBUG_PRINT("on ");
-                DEBUG_PRINTLN(i);
+                String q = String("GET /temp/");
+                q.concat(i);
+                if (header.indexOf(q) >= 0)
+                {
+                  DEBUG_PRINTF("client requesting temperature sensor %d\n", i);
+                  client.printf("%f", temperatures[i]);
+                  break;
+                }
               }
-              else if (header.indexOf(header_search + "/off") >= 0)
-              {
-                digitalWrite(pins[i], RELAY_OFF); foundcmd = true;
-                DEBUG_PRINT("off ");
-                DEBUG_PRINTLN(i);
-              }
-            }            
-*/
-            client.println("<html><body>");
-            client.println("<h1>Api</h1>");
-            client.println("/<i>port</i>/on ( activate port )<br/>");
-            client.println("/<i>port</i>/off ( deactivate port )<br/>");
-            client.println("port=0..5</br>");
-            client.println("</body></html>");
+            }
 
-            client.println();
+            if (header.indexOf("/help") >= 0)
+            {
+              client.println("<html><body>");
+              client.println("<h1>Api</h1>");
+              if (temperatureDeviceCount > 0)
+              {
+                client.println("<h3>Temperature sensors</h3>");
+                client.printf("/temp/i ( read temperature of sensor i=0..%d )", temperatureDeviceCount - 1);
+              }              
+              client.println("</body></html>");
+            }
+
             break;
           }
           else
           {
             currentLine = "";
           }
-        } else if (c != '\r')
+        }
+        else if (c != '\r')
         {
           currentLine += c;
         }
       }
     }
-
-    header = "";
+  
     client.stop();
     DEBUG_PRINTLN("Client disconnected");
   }
+  else
+  {
+    if (millis() - lastTemperatureRead > TEMPERATURE_INTERVAL_MS)
+    {
+      ReadTemperatures();
+    }
+  }
 }
 
-void printWifiData() {
+void printWifiData()
+{
   auto ip = WiFi.localIP();
   DEBUG_PRINT("IP Address: ");
   DEBUG_PRINTLN(ip);
-  
+
   byte mac[6];
   WiFi.macAddress(mac);
   DEBUG_PRINT("MAC address: ");
   for (int i = 0; i < 6; ++i)
   {
     DEBUG_PRINTHEX(mac[i]);
-    if (i != 5) DEBUG_PRINT(":");
-    else DEBUG_PRINTLN();
+    if (i != 5)
+      DEBUG_PRINT(":");
+    else
+      DEBUG_PRINTLN();
   }
 }
 
-void printCurrentNet() {
+void printCurrentNet()
+{
   DEBUG_PRINTF("SSID: %s\n", WiFi.SSID().c_str());
-  
+
   auto bssid = WiFi.BSSID();
   DEBUG_PRINT("BSSID: ");
   for (int i = 0; i < 6; ++i)
   {
     DEBUG_PRINTHEX(bssid[i]);
-    if (i != 5) DEBUG_PRINT(":");
-    else DEBUG_PRINTLN();
+    if (i != 5)
+      DEBUG_PRINT(":");
+    else
+      DEBUG_PRINTLN();
   }
-  
+
   auto rssi = WiFi.RSSI();
-  DEBUG_PRINTF("signal strength (RSSI): %ld\n", rssi);  
+  DEBUG_PRINTF("signal strength (RSSI): %ld\n", rssi);
 }
