48bb48e18aa238df6673a073893026bc755aae38
Lorenzo Delana <oss.devel@searchathing.com>
2019-01-26 12:20:02 +0100

    code cleanup

--- cathouse-controller/cathouse-controller.ino
+++ cathouse-controller/cathouse-controller.ino
@@ -70,12 +70,12 @@ void setup()
 
   setupPorts();
 
-  setupTemperatureDevices();
-
   // set led before wifi
   pinMode(LED_BUILTIN, OUTPUT);
 
   reconnectWifi();
+
+  setupTemperatureDevices();
 
   Serial.printf("SETUP DONE\n");
 }
@@ -86,136 +86,45 @@ void setup()
 
 bool systemIsOn = false;
 
-// https://github.com/esp8266/Arduino/issues/4161
-boolean WiFiReturns()
-{
-  if (WiFi.localIP() == IPAddress(0, 0, 0, 0))
-    return 0;
-  switch (WiFi.status())
-  {
-  case WL_NO_SHIELD:
-    return 0;
-  case WL_IDLE_STATUS:
-    return 0;
-  case WL_NO_SSID_AVAIL:
-    return 0;
-  case WL_SCAN_COMPLETED:
-    return 1;
-  case WL_CONNECTED:
-    return 1;
-  case WL_CONNECT_FAILED:
-    return 0;
-  case WL_CONNECTION_LOST:
-    return 0;
-  case WL_DISCONNECTED:
-    return 0;
-  default:
-    return 0;
-  }
-}
-
-auto tloop = millis();
-
-auto tWifiReconnectCheck = millis();
-auto wifiReconnecting = false;
-
 void loop()
 {
   {
-    auto t = millis();
     freeram = freeMemorySum();
     freeram_min = min(freeram, freeram_min);
-    auto tdiff = timeDiff(t, millis());
-    if (tdiff > 500)
-      Serial.printf("freeram_min took %ld\n", tdiff);
   }
 
-  if (timeDiff(tWifiReconnectCheck, millis()) > 5000 && !WiFiReturns())
-  {
-    tWifiReconnectCheck = millis();
-
-    if (!wifiReconnecting)
-    {
-      Serial.printf("wifi reconnecting\n");
-      wifiReconnecting = true;
-    }
-    reconnectWifi();
+  if (manageWifi())
     return;
-  }
-
-  wifiReconnecting = false;
-
-  {
-    auto t = millis();
-    auto res = manageWifi();
-    auto tdiff = timeDiff(t, millis());
-    if (tdiff > 500)
-      Serial.printf("manageWifi took %ld\n", tdiff);
-    if (res)
-      return;
-  }
-
-  /*
-  auto m = millis();
-  if (timeDiff(tloop, m) > 5000)
-  {
-    Serial.printf("tloop[%ld]\n", m);
-    tloop = m;
-  }*/
 
   if (!serialOsActivated)
-  {
-    auto t = millis();
     statsUpdate();
-    auto tdiff = timeDiff(t, millis());
-    if (tdiff > 500)
-      Serial.printf("statsUpdate took %ld\n", tdiff);
-  }
 
   if (!serialOsActivated)
-  {
-    auto t = millis();
     manageTemp();
-    auto tdiff = timeDiff(t, millis());
-    if (tdiff > 500)
-      Serial.printf("manageTemp took %ld\n", tdiff);
-  }
 
   if (!serialOsActivated)
-  {
-    auto t = millis();
     engineProcess();
-    auto tdiff = timeDiff(t, millis());
-    if (tdiff > 500)
-      Serial.printf("engineProcess took %ld\n", tdiff);
-  }
 
+  if (Serial.available())
   {
-    auto t = millis();
-    if (Serial.available())
+    builtinLedOff();
+
+    auto c = (char)Serial.read();
+    if ((int)c != 255)
     {
-      builtinLedOff();
-
-      auto c = (char)Serial.read();
-      if ((int)c != 255)
+      if (c == '?' && serialInput.length() == 0)
       {
-        if (c == '?' && serialInput.length() == 0)
-        {
-          printSyntaxHelp();
-          serialOsActivated = true;
-        }
-        else if (c == '\r')
-          processSerialCmd();
-        else if (c == 8) // backspace
-          serialInput.remove(serialInput.length() - 1);
-        else
-          serialInput.concat(c);
+        printSyntaxHelp();
+        serialOsActivated = true;
       }
-
-      builtinLedOn();
+      else if (c == '\r')
+        processSerialCmd();
+      else if (c == 8) // backspace
+        serialInput.remove(serialInput.length() - 1);
+      else
+        serialInput.concat(c);
     }
-    auto tdiff = timeDiff(t, millis());
-    if (tdiff > 500)
-      Serial.printf("serialOs took %ld\n", tdiff);
+
+    builtinLedOn();
   }
 }
